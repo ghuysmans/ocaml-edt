f1a7cc207b196bc31c67a7014d5f154a8ad68939
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-10-03 04:38:30 +0200

    Fix Day isomorphism

--- lib/day.ml
+++ lib/day.ml
@@ -9,20 +9,20 @@ type t =
 
 let of_string s =
   match String.lowercase_ascii s with
-  | "monday" | "lundi" -> Monday
-  | "tuesday" | "mardi" -> Tuesday
-  | "wednesday" | "mercredi" -> Wednesday
-  | "thursday" | "jeudi" -> Thursday
-  | "friday" | "vendredi" -> Friday
-  | "saturday" | "samedi" -> Saturday
-  | "sunday" | "dimanche" -> Friday
+  | "lundi" -> Monday
+  | "mardi" -> Tuesday
+  | "mercredi" -> Wednesday
+  | "jeudi" -> Thursday
+  | "vendredi" -> Friday
+  | "samedi" -> Saturday
+  | "dimanche" -> Friday
   | _ -> failwith "Day.of_string"
 
 let to_string = function
-  | Monday -> "Monday"
-  | Tuesday -> "Tuesday"
-  | Wednesday -> "Wednesday"
-  | Thursday -> "Thursday"
-  | Friday -> "Friday"
-  | Saturday -> "Saturday"
-  | Sunday -> "Sunday"
+  | Monday -> "lundi"
+  | Tuesday -> "mardi"
+  | Wednesday -> "mercredi"
+  | Thursday -> "jeudi"
+  | Friday -> "vendredi"
+  | Saturday -> "samedi"
+  | Sunday -> "dimanche"
